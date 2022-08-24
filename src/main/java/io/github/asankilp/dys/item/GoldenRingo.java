287be3c42ead9ef0850b14c60cad86a4e690cd02
Asankilp <3384816498@qq.com>
2022-08-24 23:24:31 +0800

    事1.0.2，吃三回啊三回金金金金金金金金金金林檎会召唤一个一个一个一个监守者啊啊啊啊啊啊啊啊啊啊啊

--- src/main/java/io/github/asankilp/dys/item/GoldenRingo.java
+++ src/main/java/io/github/asankilp/dys/item/GoldenRingo.java
@@ -2,11 +2,16 @@ package io.github.asankilp.dys.item;
 
 import io.github.asankilp.dys.effect.EffectReg;
 import io.github.asankilp.dys.sound.SoundReg;
+import net.minecraft.client.Game;
 import net.minecraft.network.chat.Component;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundSource;
+import net.minecraft.util.SpawnUtil;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.MobSpawnType;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.CreativeModeTab;
@@ -14,6 +19,7 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.gameevent.GameEvent;
 
 import javax.annotation.Nullable;
 import java.util.List;
@@ -48,6 +54,9 @@ public class GoldenRingo extends Item {
                 levelIn.playSound((Player) null, entityIn.getX(), entityIn.getY(), entityIn.getZ(), SoundReg.dededonSound.get(), SoundSource.AMBIENT, 0.5f, 1f);
                 entityIn.removeEffect(EffectReg.SENPAI.get());
                 entityIn.addEffect((new MobEffectInstance(MobEffects.DARKNESS, 114, 514, true, false)));
+                if (levelIn instanceof ServerLevel serverLevel) {
+                    SpawnUtil.trySpawnMob(EntityType.WARDEN, MobSpawnType.TRIGGERED, serverLevel, entityIn.blockPosition(), 20, 5, 6, SpawnUtil.Strategy.ON_TOP_OF_COLLIDER).isPresent();
+                }
             }
         } else {
             entityIn.addEffect(new MobEffectInstance(EffectReg.SENPAI.get(), 1000, 0, true, true), entityIn);
